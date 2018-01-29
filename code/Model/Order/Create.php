f1872252c30e556a20807621e3399a373248fe2a
Dennis Jessen <digidennis3@gmail.com>
2018-01-29 11:29:22 +0100

    customer email

---
+++ code/Model/Order/Create.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Adminhtml
+ * @copyright  Copyright (c) 2006-2016 X.commerce, Inc. and affiliates (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+/**
+ * Order create model
+ *
+ * @category    Mage
+ * @package     Mage_Adminhtml
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Digidennis_Sitedesign_Model_Order_Create extends Mage_Adminhtml_Model_Sales_Order_Create
+{
+
+    /**
+     * Create new order
+     *
+     * @return Mage_Sales_Model_Order
+     */
+    public function createOrder()
+    {
+        $this->_prepareCustomer();
+        $this->_validate();
+        $quote = $this->getQuote();
+        $this->_prepareQuoteItems();
+
+        $service = Mage::getModel('sales/service_quote', $quote);
+        /** @var Mage_Sales_Model_Order $oldOrder */
+        $oldOrder = $this->getSession()->getOrder();
+        if ($oldOrder->getId()) {
+            $originalId = $oldOrder->getOriginalIncrementId();
+            if (!$originalId) {
+                $originalId = $oldOrder->getIncrementId();
+            }
+            $orderData = array(
+                'original_increment_id'     => $originalId,
+                'relation_parent_id'        => $oldOrder->getId(),
+                'relation_parent_real_id'   => $oldOrder->getIncrementId(),
+                'edit_increment'            => $oldOrder->getEditIncrement()+1,
+                'increment_id'              => $originalId.'-'.($oldOrder->getEditIncrement()+1)
+            );
+            $quote->setReservedOrderId($orderData['increment_id']);
+            $service->setOrderData($orderData);
+
+            $oldOrder->cancel();
+        }
+
+        /** @var Mage_Sales_Model_Order $order */
+        $order = $service->submit();
+        $customer = $quote->getCustomer();
+        if ((!$customer->getId() || !$customer->isInStore($this->getSession()->getStore()))
+            && !$quote->getCustomerIsGuest()
+        ) {
+            $customer->setCreatedAt($order->getCreatedAt());
+            $customer->save();
+        }
+        if ($oldOrder->getId()) {
+            $oldOrder->setRelationChildId($order->getId());
+            $oldOrder->setRelationChildRealId($order->getIncrementId());
+            $oldOrder->save();
+            $order->save();
+        }
+        if ($this->getSendConfirmation()) {
+            $order->queueNewOrderEmail();
+        }
+
+        Mage::dispatchEvent('checkout_submit_all_after', array('order' => $order, 'quote' => $quote));
+
+        return $order;
+    }
+
+}
